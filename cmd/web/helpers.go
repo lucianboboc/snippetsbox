68f4571c23e1b3b500c3bb24fd4b7638fd3b1e70
Lucian Boboc <info@lucianboboc.com>
2024-06-17 22:27:25 +0200

    CSRF

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/go-playground/form/v4"
+	"github.com/justinas/nosurf"
 	"log/slog"
 	"net/http"
 	"runtime/debug"
@@ -56,6 +57,7 @@ func (app *application) newTemplateData(r *http.Request) templateData {
 		CurrentYear:     time.Now().Year(),
 		Flash:           app.sessionManager.PopString(r.Context(), "flash"),
 		IsAuthenticated: app.isAuthenticated(r),
+		CSRFToken:       nosurf.Token(r),
 	}
 }
 
